0728c7d82bbc25fb636cfb98086044c402c9efcf
Karan Sharma <s.karansharma01@gmail.com>
2017-01-08 13:03:03 -0500

    * Adding comments * Moving csv files to data folder * Final touches

--- BillingEngine-ShinyApp/app.R
+++ BillingEngine-ShinyApp/app.R
@@ -8,43 +8,51 @@
 #
 
 library(shiny)
+library(plotly)
+library(data.table)
+
+BEData <- as.data.frame(read.csv('/Users/karansharma/Google Drive/DataScienceProjects/BillingEngine-ShinyApp/Data/BillingEngineData.csv', header = TRUE, sep = ',') )
+library(data.table)
+BEData <- as.data.table(BEData)
+Date <- as.Date(BEData$Date,'%m/%d/%y')
+BEData[,Date:=NULL]
+BEData[,Date.1:=NULL]
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
+  
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
+   titlePanel("Billing Engine Dashboard"),
+   # Dropdown with input from the BillingEngine columns  
    sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
+   sidebarPanel(
+      selectInput("billingTrend","\n\n\n Please select the value:",
+                  choices = colnames(BEData), selected = "Total Revenue")
+   ),
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotlyOutput("distPlot")
       )
    )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-}
 
+# Define server logic required to draw a plot
+server <- function(input, output) {
+    
+    output$distPlot <- renderPlotly({
+      Value <- BEData[[input$billingTrend]]
+      ggplot(data = BEData, aes(x = Date, y = Value, group=1)) + 
+        geom_point() +
+        geom_line() +
+        geom_smooth(se = F) +
+        ggtitle("ShareFile Billing Trend for SMB") +
+        xlab("\n\nTime")+
+        ylab(paste(input$billingTrend,"\n")) +
+        theme(text = element_text(size=8), axis.title.y=element_text(margin=margin(0,80,0,0)))
+  })
+    
+}
 # Run the application 
 shinyApp(ui = ui, server = server)
-getwd()
